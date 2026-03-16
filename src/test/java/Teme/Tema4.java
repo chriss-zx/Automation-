cdc23038fc5efa4cc141663eed75aa960f804365
cristi <c.nacea21@gmail.com>
2026-03-16 18:29:29 +0200

    update

--- src/test/java/Teme/Tema4.java
+++ src/test/java/Teme/Tema4.java
@@ -1,3 +1,5 @@
+package Teme;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -5,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -32,10 +33,10 @@ public class Tema4 {
     }
 
 
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+//    @AfterMethod
+//    public void tearDown() {
+//        driver.quit();
+//    }
 
 
 //    1. Creează un test care:
